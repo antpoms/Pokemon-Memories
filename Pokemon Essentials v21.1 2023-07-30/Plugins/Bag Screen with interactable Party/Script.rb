93d0d4d153579d996e3e046d0b13c24033c33751
antpoms <148887171+antpoms@users.noreply.github.com>
2025-12-07 12:33:36 +0100

    vendeurs

--- Pokemon Essentials v21.1 2023-07-30/Plugins/Bag Screen with interactable Party/Script.rb
+++ Pokemon Essentials v21.1 2023-07-30/Plugins/Bag Screen with interactable Party/Script.rb
@@ -553,13 +553,13 @@ class PokemonBag_Scene
     @sprites["panorama"].x -= 2 if BagScreenWiInParty::PANORAMA == true
   end
 
-  def pbStartScene(bag, party, choosing = false, filterproc = nil, resetpocket = true)
+  def pbStartScene(bag, choosing = false, filterproc = nil, resetpocket = true)
     @viewport   = Viewport.new(0, 0, Graphics.width, Graphics.height)
     @viewport.z = 99999
     @bag        = bag
     @choosing   = choosing
     @filterproc = filterproc
-    @party      = party
+    @party      = $player.party
     
     pbRefreshFilter
     lastpocket = @bag.last_viewed_pocket
